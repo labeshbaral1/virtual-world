56b5c1f51603d0dd7fb1409eb0a89eedb90096f7
Atri-lab <atrivyas7@gmail.com>
2024-04-08 20:58:19 -0400

    Implemented Occupancy Indicator

--- 8. Saving The World/js/markings/start.js
+++ 8. Saving The World/js/markings/start.js
@@ -1,18 +1,24 @@
 class Start extends Marking {
-   constructor(center, directionVector, width, height) {
+   constructor(center, directionVector, width, height, occupancy = 1) {
       super(center, directionVector, width, height);
 
       this.img = new Image();
       this.img.src = "car.png";
       this.type = "start";
+      this.occupancy = occupancy;
    }
 
    draw(ctx) {
       ctx.save();
       ctx.translate(this.center.x, this.center.y);
       ctx.rotate(angle(this.directionVector) - Math.PI / 2);
-      
+
       ctx.drawImage(this.img, -this.img.width / 2, -this.img.height / 2);
+
+      // Draw occupancy indicator
+      ctx.fillStyle = "black";
+      ctx.font = "bold 16px Arial";
+      ctx.fillText(`  ${this.occupancy}`, -this.img.width / 2, -this.img.height / 2 - 10);
 
       ctx.restore();
    }
